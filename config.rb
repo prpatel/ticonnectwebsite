4379722244deaa1821ba3a520f6e1676344a25ed
pratik patel <pratik@mypatelspace.com>
2014-08-13 16:23:10 -0400

    updated Ti-COnnect website with new ConnectJS template

---
+++ config.rb
@@ -0,0 +1,22 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+require "zurb-foundation"
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "a/c/"
+sass_dir = "scss"
+images_dir = "a/i/"
+javascripts_dir = "a/j/"
+fonts_dir = "a/f/"
+
+output_style = :nested
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
+color_output = false
+
+preferred_syntax = :scss
